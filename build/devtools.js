64c33538261354b04712a771877ca56883ad71eb
Justin Lo <justinkevinlo@gmail.com>
2019-11-25 15:27:30 -0800

    working on retrieving http response object

--- build/devtools.js
+++ build/devtools.js
@@ -1,3 +1,8 @@
 chrome.devtools.panels.create("Artimes", null, "panel.html", panel =>
-  console.log("hello world")
+  console.log("hello world");
 );
+
+chrome.devtools.network.onRequestFinished.addListener((httpReq) => {
+  // grab req/ res object and update state to display past queries
+  console.log(httpReq);
+})
--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const App = (props) => {
+  console.log('i am in useEffect');
+  useEffect(() => {
+    // chrome.devtools.network.onRequestFinished.addListener((httpReq) => {
+    //   console.log('i am in useEffect');
+    //   console.log(JSON.parse(httpReq.request.postData.text));
+    // })
+  })
+
   return (
     <div>
       Hello World;
