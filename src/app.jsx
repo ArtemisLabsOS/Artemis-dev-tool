283d0f60d585cbcd97b60e93f5910589abd1839d
Justin Lo <justinkevinlo@gmail.com>
2019-11-30 18:46:15 -0800

    successfully attained get request end point, schema introspection not working yet

--- src/app.jsx
+++ src/app.jsx
@@ -3,10 +3,18 @@ import bglog from "./utils/bglog.js";
 import QueryContainer from "./containers/QueryContainer.jsx";
 import "./stylesheets/style.scss";
 import ResultDisplay from "./components/GraphQLResponse.jsx"
-
+const { introspectionQuery } = require('graphql');
+const {
+  introspect,
+  introspectURL,
+  introspectFile 
+} = require('graphql-introspect');
+// const http = require('http');
+// console.log(http);
 const App = props => {
   const [queries, updateQueries] = useState([]);
   const [results, updateResults] = useState([]);
+  // const [schemas, updateSchema] = useState([]);
 
   useEffect(() => {
     chrome.devtools.network.onRequestFinished.addListener((httpReq) => {
@@ -31,10 +39,30 @@ const App = props => {
     });
   },[]);
 
-  
-    // chrome.devtools.network.onRequestFinished.addListener((httpRequest) => {
-    //   bglog('THIS IS THE REQUEST BODY', httpRequest);
-    // })
+  useEffect(() => {
+    chrome.devtools.network.onRequestFinished.addListener((httpReq) => {
+      bglog('this is the second useEffect http request');
+      // httpReq.getContent((res) => {
+        // bglog(res);
+      // })
+      // bglog(httpReq.request.postData.text);
+
+      // httpReq.request.url gets us the http end point
+      console.log(httpReq.request);
+      console.log(httpReq.request.url);
+      
+      introspect(httpReq.request.url)
+      // .then((output) => {
+      //   // output is supposed to be the schema
+        
+      //   console.log(output.json());
+      //   return output.json();
+      //   console.log('hi');
+      // })
+      .then((output) => output.json())
+      .catch((err) => console.log('I AM ERROR', err));
+    })
+  }, [schemas])
   
 
   bglog(['this is queries', queries]);
