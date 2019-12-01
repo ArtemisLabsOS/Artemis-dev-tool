46d3ffcd4e2eb1e3101c7097a440f5880f65f838
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2019-11-30 18:06:24 -0800

    23

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,7 @@ import QueryContainer from "./containers/QueryContainer.jsx";
 import ResponseContainer from './containers/ResponseContainer.jsx'
 import "./stylesheets/style.scss";
 
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider } from 'react-apollo-hooks';
 
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
@@ -14,15 +14,19 @@ const httpLink = new HttpLink({
   uri: 'https://api.spacex.land/graphql/',
 
 });
+console.log("this is the link")
+console.log(httpLink)
 
 const client = new ApolloClient({
 	link: httpLink,
   cache: new InMemoryCache(),
 });
+console.log("this is client")
+console.log(client)
 
 const App = props => {
   const [queries, updateQueries] = useState([]);
-  const [results, updateResults] = usçeState([]);
+  const [results, updateResults] = useState([]);
   // const [cache, updateCache] = useState([]);
 
   useEffect(() => {
@@ -56,24 +60,27 @@ const App = props => {
     <div id="containers">
       <QueryContainer queries={queries} />
       <ResponseContainer results={results} />
-      console.log('client with caching is:')
+      {console.log('client with caching is:'+client)}
       <ApolloProvider client={client} cache={client.cache}>
-						<div
-							css={{
-								display: 'grid',
-								gridTemplateColumns: '80px repeat(auto-fit, 300px)',
-								alignItems: 'start',
-								height: 'calc(100vh - 4px)',
-								overflow: 'hidden',
-							}}
-						>
-						
-						</div>
-					</ApolloProvider>
-
+      <div
+        css={{
+          display: 'grid',
+          gridTemplateColumns: '80px repeat(auto-fit, 300px)',
+          alignItems: 'start',
+          height: 'calc(100vh - 4px)',
+          overflow: 'hidden',
+        }}
+      >
+      console.log({client})
+      </div>
+      </ApolloProvider>
     </div>
   );
 };
+
+
+
+
 
 function IsJsonString(str) {
   try {
