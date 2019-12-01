c515d93c9e798f2f8deebb5a97b14c78367fec1e
timjj92 <timjj92@gmail.com>
2019-11-30 23:41:04 -0800

    background msg update

--- build/contentScript.js
+++ build/contentScript.js
@@ -2,19 +2,14 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   switch(msg.msg){
     case "getDOM":
-      console.log(document.getElementsByTagName('body')[0].innerHTML);
       sendResponse({msg: document.getElementsByTagName('body')[0].innerHTML});
       break;
+    case "rerenderDOM":
+      document.getElementsByTagName('body')[0].innerHTML = msg.newBody;
+      sendResponse({msg: "rerender done"});
+      break;
   }
-  console.log(msg);
 });
-
-//send message
-const sendMsg = (msg) =>{
-  chrome.runtime.sendMessage({greeting: msg}, function(response) {
-  console.log(response.farewell);
-  })
-};
 
 console.log("hello from contentScript");
 
